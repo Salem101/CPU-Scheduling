145d15d00be8d2b74b916f8da67df2a66edcfe81
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2022-08-10 22:18:05 -0400

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -1,3 +1,22 @@
+//Rivka Salem 8/22
+
+#ifndef CPU_OSLABS_H
+#define CPU_OSLABS_h
+#define QUEUEMAX 10
+#define MAPMAX 10
+#define TABLEMAX 10
+#define POOLMAX 10
+#define REFERENCEMAX 20
+#define MAX(a,b) ((a>b)?a:b)
+#define MIN(a,b) ((a>b)?b:a)
+
+struct RCB {
+        int request_id;
+        int arrival_timestamp;
+        int cylinder;
+        int address;
+        int process_id;
+    };
 
 
 struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp)
