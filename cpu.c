42f1b5606b721870ae1ea09457a40fb3c32acd26
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2022-08-11 21:12:46 -0400

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -57,28 +57,22 @@ struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queu
 }
 struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp)
 {
-        if(current_process.process_id == 0)
-        { return current_process; }
-         
+   return NULL;
 }
 struct PCB handle_process_arrival_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int time_stamp)
 {
-            if(current_process.process_id == 0)
-        { return current_process; }
+   return NULL;
 }
 struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp)
 {
-            if(current_process.process_id == 0)
-        { return current_process; }
+   return NULL;
 }
 struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp, int time_quantum)
 {
-            if(current_process.process_id == 0)
-        { return current_process; }
+   return NULL;
 }
 struct PCB handle_process_completion_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp, int time_quantum)
 {
-            if(current_process.process_id == 0)
-        { return current_process; }
+   return NULL;
 }
 
